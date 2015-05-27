0bf46195f434b773d5de859a2a038449f8bd1121
Tanakorn Leesatapornwongsa <tanakorn@cs.uchicago.edu>
2015-05-26 18:03:53 -0600

    Fix bugs in deployment script

--- vanilla/scbin/gen_nmc_machines.sh
+++ vanilla/scbin/gen_nmc_machines.sh
@@ -9,6 +9,8 @@ numnode=$1
 rm machinelist 2> /dev/null
 i=1
 while [ $i -le $numnode ]; do
-  echo $i node-$i.scale.ucare >> machinelist
+  #echo $i node-$i.scale.ucare >> machinelist
+  ip=`nslookup node-$i.scale.ucare | awk 'BEGIN { flag = 0 } /Name/ { flag = 1 } /Address/ { if (flag == 1) print $2 }'`
+  echo $i $ip >> machinelist
   i=`expr $i + 1`
 done
