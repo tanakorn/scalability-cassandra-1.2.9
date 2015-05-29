14ebc874f60f911af5e2c6979f57d211656c4612
Tanakorn Leesatapornwongsa <tanakorn@cs.uchicago.edu>
2015-05-29 17:18:29 -0600

    Change gen_nmc_machines to generate IP address list

--- vanilla/scbin/gen_nmc_machines.sh
+++ vanilla/scbin/gen_nmc_machines.sh
@@ -5,12 +5,18 @@ if [ ! $1 ]; then
   exit 1
 fi
 
+down=13
+
 numnode=$1
 rm machinelist 2> /dev/null
 i=1
+j=1
 while [ $i -le $numnode ]; do
   #echo $i node-$i.scale.ucare >> machinelist
-  ip=`nslookup node-$i.scale.ucare | awk 'BEGIN { flag = 0 } /Name/ { flag = 1 } /Address/ { if (flag == 1) print $2 }'`
-  echo $i $ip >> machinelist
-  i=`expr $i + 1`
+  if [ $j -ne $down ]; then
+    ip=`nslookup node-$j.scale.ucare | awk 'BEGIN { flag = 0 } /Name/ { flag = 1 } /Address/ { if (flag == 1) print $2 }'`
+    echo $i $ip >> machinelist
+    i=`expr $i + 1`
+  fi
+  j=`expr $j + 1`
 done
