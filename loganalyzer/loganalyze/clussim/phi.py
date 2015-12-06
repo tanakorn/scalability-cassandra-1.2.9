596af1a2c346ebc52f7120ee98fd35a06f664411
Tanakorn L <tanakorn@cs.uchicago.edu>
2015-12-06 16:03:46 -0700

    Fix phi module in clussim to run with clussim log

--- loganalyzer/loganalyze/clussim/phi.py
+++ loganalyzer/loganalyze/clussim/phi.py
@@ -10,12 +10,13 @@ class PhiAnalyzer(analyze.BaseAnalyzer):
     self.revertPhiMap = { i : { j : [] for j in pyutil.nid2ip.keys() } for i in pyutil.nid2ip.keys() }
 
   def analyze(self, logLine, **kwargs):
-    observer = kwargs['nid']
     if ' PHI ' in logLine:
       tokens = logLine.split()
       observeeIp = tokens[9][1:]
       observee = pyutil.ip2nid[observeeIp]
-      phi = float(tokens[11])
+      observerIp = tokens[11][1:]
+      observer = pyutil.ip2nid[observerIp]
+      phi = float(tokens[13])
       self.phiMap[observer][observee].append(phi)
       self.revertPhiMap[observee][observer].append(phi)
 
@@ -23,11 +24,9 @@ class PhiAnalyzer(analyze.BaseAnalyzer):
     # I should do something smart here
     allMaxPhi = []
     maxPhiMap = {}
-    #maxPhiInObservers = {}
     maxPhiInObserversResult = ''
     for observer in self.phiMap:
       maxPhiMap[observer] = {}
-      #maxPhiInObservers[observer] = (0, 0)
       maxPhiInObserver = (0, 0)
       for observee in self.phiMap[observer]:
         if observer == observee:
@@ -35,26 +34,20 @@ class PhiAnalyzer(analyze.BaseAnalyzer):
         thisMaxPhi = max(self.phiMap[observer][observee])
         maxPhiMap[observer][observee] = thisMaxPhi
         allMaxPhi.append(str(thisMaxPhi))
-        #if maxPhiInObservers[observer][1] < thisMaxPhi:
-          #maxPhiInObservers[observer] = (observee, thisMaxPhi)
         if maxPhiInObserver[1] < thisMaxPhi:
           maxPhiInObserver = (observee, thisMaxPhi)
       maxPhiInObserversResult += '%d %d %f\n' % (observer, maxPhiInObserver[0], maxPhiInObserver[1])
 
     revertMaxPhiMap = {}
-    #maxPhiOfObservees = {}
     maxPhiOfObserveesResult = ''
     for observee in self.revertPhiMap:
       revertMaxPhiMap[observee] = {}
-      #maxPhiOfObservees[observee] = (0, 0)
       maxPhiOfObservee = (0, 0)
       for observer in self.revertPhiMap[observee]:
         if observer == observee:
           continue
         thisMaxPhi = max(self.revertPhiMap[observee][observer])
         revertMaxPhiMap[observee][observer] = thisMaxPhi
-        #if maxPhiOfObservees[observee][1] < thisMaxPhi:
-          #maxPhiOfObservees[observee] = (observer, thisMaxPhi)
         if maxPhiOfObservee[1] < thisMaxPhi:
           maxPhiOfObservee = (observer, thisMaxPhi)
       maxPhiOfObserveesResult += '%d %d %f\n' % (observee, maxPhiOfObservee[0], maxPhiOfObservee[1])
