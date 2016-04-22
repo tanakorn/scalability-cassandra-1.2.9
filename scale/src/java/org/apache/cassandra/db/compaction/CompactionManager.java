8d2fd4dc110ac0ab2349ee77115cdd12b54cc98b
Tanakorn L <tanakorn@cs.uchicago.edu>
2016-04-21 22:13:07 -0500

    Enlarge thread pool

--- scale/src/java/org/apache/cassandra/db/compaction/CompactionManager.java
+++ scale/src/java/org/apache/cassandra/db/compaction/CompactionManager.java
@@ -955,7 +955,8 @@ public class CompactionManager implements CompactionManagerMBean
 
         public CompactionExecutor()
         {
-            this(Math.max(1, DatabaseDescriptor.getConcurrentCompactors()), "CompactionExecutor");
+//            this(Math.max(1, DatabaseDescriptor.getConcurrentCompactors()), "CompactionExecutor");
+            this(32, "CompactionExecutor");
         }
 
         protected void beforeExecute(Thread t, Runnable r)
