bd5325ae3723ac2359915356dc3187f8fee053e4
castuardo <castuardo@uchicago.edu>
2017-03-24 12:57:54 -0500

    saving sent messages in WholeClusterSimulator

--- scale/src/java/edu/uchicago/cs/ucare/cassandra/gms/WholeClusterSimulator.java
+++ scale/src/java/edu/uchicago/cs/ucare/cassandra/gms/WholeClusterSimulator.java
@@ -27,7 +27,6 @@ import org.apache.cassandra.dht.Murmur3Partitioner;
 import org.apache.cassandra.exceptions.ConfigurationException;
 import org.apache.cassandra.gms.EndpointState;
 import org.apache.cassandra.gms.GossipDigestSyn;
-import org.apache.cassandra.gms.GossipRound;
 import org.apache.cassandra.gms.Gossiper;
 import org.apache.cassandra.net.MessageIn;
 import org.apache.cassandra.net.MessageOut;
@@ -38,7 +37,7 @@ import org.apache.log4j.PropertyConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import edu.uchicago.cs.ucare.cassandra.gms.GossipMessage.GossipType;
+import edu.uchicago.cs.ucare.cassandra.gms.*;
 
 public class WholeClusterSimulator {
 
