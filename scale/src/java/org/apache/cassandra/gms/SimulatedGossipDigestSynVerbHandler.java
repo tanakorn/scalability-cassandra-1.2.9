b37c034ee0903c299d82c838717423c91f7f245a
Tanakorn L <tanakorn@cs.uchicago.edu>
2017-04-06 21:18:57 -0500

    Fix compliation error

--- scale/src/java/org/apache/cassandra/gms/SimulatedGossipDigestSynVerbHandler.java
+++ scale/src/java/org/apache/cassandra/gms/SimulatedGossipDigestSynVerbHandler.java
@@ -136,8 +136,8 @@ public class SimulatedGossipDigestSynVerbHandler implements IVerbHandler<GossipD
         // TODO Can I comment this out?
         Gossiper.instance.checkSeedContact(from);
         gDigestAckMessage.createdTime = System.currentTimeMillis();
-        WholeClusterSimulator.msgQueues.get(from).add(gDigestAckMessage);
-//        WholeClusterSimulator.msgQueue.add(gDigestAckMessage);
+//        WholeClusterSimulator.msgQueues.get(from).add(gDigestAckMessage);
+        WholeClusterSimulator.msgQueue.add(gDigestAckMessage);
     }
 
     /*
